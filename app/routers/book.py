bf1b6ece5e2fe45f977176a344704b3c0af89e44
ceciliaass <ceciliasantos11@hotmail.com>
2025-11-24 20:55:19 -0300

    fix rotas de book

--- app/routers/book.py
+++ app/routers/book.py
@@ -14,6 +14,28 @@ def lista_books(db: Session = Depends(get_db)):
     return book.get_books(db)
 
 
+@router.get("/top-rated", response_model=list[BookSchema])
+def obter_melhores_livros(
+    quant: Optional[int] = Query(
+        5,
+        description="Quantidade de livros a serem listados"
+    ),
+    db: Session = Depends(get_db)
+):
+    """Retorna os n livros com melhor avaliação, por padrão, os 5 melhores"""
+    return book.get_best_book(quant, db)
+
+
+@router.get("/price-range", response_model=list[BookSchema])
+def obter_livros_na_faixa_de_preco(
+    min: float = Query(description="Menor preço"),
+    max: float = Query(description="Maior preço"),
+    db: Session = Depends(get_db)
+):
+    """Obtem os livros que estão em uma determinada faixa de preço"""
+    return book.get_books_between_prices(db, min, max)
+
+
 @router.get("/search", response_model=list[BookSchema])
 def pesquisar_books(
     title: Optional[str] = Query(None, description="Título do livro"),
@@ -35,23 +57,3 @@ def obter_book(
     """Busca por um livro especifico com base em seu ID"""
     return book.get_book(book_id, db)
 
-@router.get("/top-rated", response_model=list[BookSchema])
-def obter_melhores_livros(
-    quant: Optional[int] = Query(
-        5,
-        description="Quantidade de livros a serem listados"
-    ),
-    db: Session = Depends(get_db)
-):
-    """Retorna os n livros com melhor avaliação, por padrão, os 5 melhores"""
-    return book.get_best_book(quant, db)
-
-
-@router.get("/price-range", response_model=list[BookSchema])
-def obter_livros_na_faixa_de_preco(
-    min: float = Query(description="Menor preço"),
-    max: float = Query(description="Maior preço"),
-    db: Session = Depends(get_db)
-):
-    """Obtem os livros que estão em uma determinada faixa de preço"""
-    return book.get_books_between_prices(db, min, max)
